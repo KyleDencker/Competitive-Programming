13e28ae007799f6b52999169583aeaf311bd1d97
AhmadBarhamje <ahmadbarhamje@hotmail.com>
2016-02-26 09:39:36 -0500

    Update D.java

--- CodeForces/Educational Round 5/D.java
+++ CodeForces/Educational Round 5/D.java
@@ -2,41 +2,61 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
+
+//Ahmad Barhamje
 
 public class D {
-
 	public static void main(String[] args) throws IOException {
-		BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader f = new BufferedReader(new InputStreamReader(System.in));
 		PrintWriter out = new PrintWriter(System.out);
-		StringTokenizer tokens = new StringTokenizer(stdin.readLine());
-
-		int n = Integer.parseInt(tokens.nextToken());
-		int k = Integer.parseInt(tokens.nextToken());
-		
-		int start_index = 1;
-		int end_index = 1;
-		int longest = 1;
-		int current = 1;
-		int last_num = -1;
-		int index = 1;
-		
-
-		
-		tokens = new StringTokenizer(stdin.readLine());
-		last_num = Integer.parseInt(tokens.nextToken());
-		ArrayList<Integer> used = new ArrayList<Integer>();
-		used.add(last_num);
-		
-		while (tokens.hasMoreTokens()) {
-			int temp = Integer.parseInt(tokens.nextToken());
-						
+		String[] split = f.readLine().split(" ");
+		int n = Integer.parseInt(split[0]), k = Integer.parseInt(split[1]);
+		int[] a = new int[n];
+		split = f.readLine().split(" ");
+		for(int i = 0; i < split.length; i++) {
+			a[i] = Integer.parseInt(split[i]);
 		}
-		out.println(start_index + " " + end_index);
-		out.flush();
+		
+		int l = 0, r = -1, c = 0;
+		int len = 1, fL = 0, fR = 0;
+		int[] freq = new int[1000001];
+		
+		for(int i = 0; i < n; i++) {
+			boolean runs = false;
+			if(c < k) { //can add more distinct numbers
+				runs = true;
+				if(freq[a[i]] == 0) { //is a distinct number
+					freq[a[i]]++;
+					c++;
+					if(r+1 < n) {
+						r++;
+					}
+				} else { //duplicate number
+					freq[a[i]]++;
+					if(r+1 < n) {
+						r++;
+					}
+				}
+			} 
+			if(c == k || i == a.length-1){ //no more distinct numbers are available for use
+				//count num of duplicates after last number
+				while(r+1 < n && freq[a[r+1]] > 0) { 
+					freq[a[r+1]]++;
+					i++;
+					r++;
+				}
+				int curLen = (r-l)+1;
+				if(curLen > len) {
+					len = curLen;
+					fL = l; fR = r;
+				}
+				freq[a[l]]--;
+				if(freq[a[l]] == 0) c--;
+				l++;
+				if(!runs) i--;
+			}
+		}
+		out.println((fL+1) + " " + (fR+1));
 		out.close();
-		
-		
 	}
 }
