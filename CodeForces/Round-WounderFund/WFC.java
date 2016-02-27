a494fdc6887cfaaa85162615bdaeecb06536ff61
Kyle Dencker <kdencker@gmail.com>
2016-02-26 23:18:03 -0500

    Cleaned up Codeforces competitions

--- CodeForces/Round-WounderFund/WFC.java
+++ CodeForces/Round-WounderFund/WFC.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.StringTokenizer;
@@ -13,58 +14,50 @@ public class WFC {
 		PrintWriter out = new PrintWriter(System.out);
 		int points = Integer.parseInt(stdin.readLine());
 		ArrayList<Point> listOfPoints = new ArrayList<Point>();
-		int order = 1;
-		for (int i=0; i<points; i++) {
-			StringTokenizer tokens = new StringTokenizer(stdin.readLine());
-			listOfPoints.add(new Point(Integer.parseInt(tokens.nextToken()), Integer.parseInt(tokens.nextToken()), order));
+		int order = 2;
+		StringTokenizer tokens = new StringTokenizer(stdin.readLine());
+		Point orgin = new Point(Integer.parseInt(tokens.nextToken()), Integer.parseInt(tokens.nextToken()), 1, null);
+		for (int i=1; i<points; i++) {
+			tokens = new StringTokenizer(stdin.readLine());
+			listOfPoints.add(new Point(Integer.parseInt(tokens.nextToken()), Integer.parseInt(tokens.nextToken()), order, orgin));
 			order++;
 		}
 		Collections.sort(listOfPoints);
-		int x = listOfPoints.get(0).x;
-		boolean twoX = false;
-		boolean two = false;
-		String answer;
-		answer = listOfPoints.get(0).order + " ";
-
-		for (int i=1; i<listOfPoints.size(); i++) {
-			if (x == listOfPoints.get(i).x) {
-				if (twoX) {
-					continue;
-				} else {
-					twoX = true;
-					two = true;
-					answer += listOfPoints.get(i).order + " ";
-					continue;
-				}
+		ArrayList<Point> answer = new ArrayList<Point>();
+		answer.add(orgin);
+		for (int i=0; i<listOfPoints.size(); i++) {
+			if (listOfPoints.get(i).x != orgin.x) {
+				answer.add(listOfPoints.get(i));
+				listOfPoints.remove(i);
+				break;
 			}
-			
-			if (two==true) {
-				out.println(answer+listOfPoints.get(i).order);
-				out.flush();
-				out.close();
-				return;
-			} else {
-				two = true;
-				answer += listOfPoints.get(i).order + " ";
+		}
+		
+		// ax+by+c = 0
+		// y = m * x + c
+		// m = a / b;
+		// a = y1 - y2
+		// b = x1 - x2
+		// c = y - mx ;
+		int a = answer.get(0).y - answer.get(1).y;
+		int b = answer.get(0).x - answer.get(1).x;
+		double m = (double) a / b;
+		double c = orgin.y - m * orgin.x; 
+		
+		for (int i=0; i<listOfPoints.size(); i++) {
+			if (listOfPoints.get(i).y != m * listOfPoints.get(i).x + c) {
+				answer.add(listOfPoints.get(i));
+				break;
 			}
-			
+		}
+		
+		for (int i=0; i<3; i++) {
+			System.out.print(answer.get(i).order + " ");
 		}
 
-	}
-	
-	public static float sign (Point p1, Point p2, Point p3){
-	    return (p1.x - p3.x) * (p2.y - p3.y) - (p2.x - p3.x) * (p1.y - p3.y);
-	}
 
-	public static Boolean PointInTriangle (Point pt, Point v1, Point v2, Point v3)
-	{
-		Boolean b1, b2, b3;
 
-	    b1 = sign(pt, v1, v2) <= 0.0f;
-	    b2 = sign(pt, v2, v3) <= 0.0f;
-	    b3 = sign(pt, v3, v1) <= 0.0f;
 
-	    return ((b1 == b2) && (b2 == b3));
 	}
 
 }
@@ -73,18 +66,29 @@ class Point implements Comparable<Point> {
 	int x; 
 	int y;
 	int order;
-	
-	public Point(int x, int y, int o) {
+	BigInteger distance;
+
+	public Point(int x, int y, int o, Point orgin) {
 		this.x = x;
 		this.y = y;
 		this.order = o;
+		if (orgin != null) {
+			distance = distance(orgin);
+			//System.out.println(order + " " + distance.toString());
+
+		}
+	}
+
+	public BigInteger distance(Point o) {
+		BigInteger deltaX = new BigInteger((o.x-this.x)+"");
+		deltaX = deltaX.multiply(deltaX);
+		BigInteger deltaY = new BigInteger((o.y-this.y)+"");
+		deltaY = deltaY.multiply(deltaY);
+		return deltaX.add(deltaY);
 	}
 
 	@Override
 	public int compareTo(Point e) {
-		if (e.x == this.x) {
-			return e.y - this.y;
-		}
-		return (e.x - this.x);
+		return -1*e.distance.compareTo(this.distance);
 	}
 }
