748b7555803b6da7bb01941eff987bed659e8879
Enrique Rodriguez <soraestafar@gmail.com>
2018-05-02 11:15:15 -0400

    Solution to B

--- CodeForces/Educational Round 39/Educational_Round_39B.java
+++ CodeForces/Educational Round 39/Educational_Round_39B.java
@@ -5,17 +5,17 @@ public class Educational_Round_39B {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		long n = scan.nextLong(), m = scan.nextLong();  //Scan in input
-		for (;;) {										//Loop infinitely since we don't know how long process will take
-			if (n == 0 || m == 0)						//Constraint listed in problem
+		for (;;) {					//Loop infinitely since we don't know how long process will take
+			if (n == 0 || m == 0)			//Constraint listed in problem
 				break;
-			else if (n >= m * 2)						//We use % here because - would give us a TLE error
+			else if (n >= m * 2)			//We use % here because - would give us a TLE error
 				n %= m * 2;
 			else if (m >= n * 2)
-				m %= n * 2;								//We use % here because - would give us a TLE error
+				m %= n * 2;			//We use % here because - would give us a TLE error
 			else
-				break;									//If we can't do process anymore, end the program
+				break;				//If we can't do process anymore, end the program
 		}
-		System.out.println(n + " " + m);				//Printing
-		scan.close();									//Close resource leak
+		System.out.println(n + " " + m);		//Printing
+		scan.close();					//Close resource leak
 	}		
 }
