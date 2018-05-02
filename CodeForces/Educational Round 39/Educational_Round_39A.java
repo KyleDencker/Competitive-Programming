45b14e40f0d63436ce49cae6fab19dfef97c617c
Enrique Rodriguez <soraestafar@gmail.com>
2018-05-02 11:11:00 -0400

    Solution to A

---
+++ CodeForces/Educational Round 39/Educational_Round_39A.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Educational_Round_39A {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();					//Scan in input
+		int total = 0;							//Counter
+		for (int i = 0; i < n; i++) {
+			total += Math.abs(scan.nextInt());	//Loop n times, adding the absolute value of the next scanned number to total
+		}
+		System.out.println(total);				//Print total
+		scan.close();							//Close resource leak
+	}
+}
