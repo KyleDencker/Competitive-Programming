592842fc3da58837d3eb75de11735e73d06f2056
Enrique Rodriguez <soraestafar@gmail.com>
2018-05-07 11:43:19 -0400

    Update R479D.java

--- CodeForces/Round-479/R479D.java
+++ CodeForces/Round-479/R479D.java
@@ -2,7 +2,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Scanner;
-
+/*
+ *@Lior on Codeforces
+ */
 public class R479D {
 
 	public static void main(String[] args) {
